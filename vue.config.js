82f202e53fbfb28e683cb478376bdd1adbb63b13
smwbtech <smwb.tech@gmail.com>
2019-08-26 00:00:35 +0300

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+	css: {
+		extract: false
+	},
+	configureWebpack: {
+		mode: 'production',
+		plugins: [new TerserPlugin()]
+	}
+};
