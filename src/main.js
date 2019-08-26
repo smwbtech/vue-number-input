8bb3d2dd06e0c2d1eacc773dbf4a222b7dd8ac30
smwbtech <smwb.tech@gmail.com>
2019-08-27 00:25:07 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueAxe from 'vue-axe';
+import VueHighlightJS from 'vue-highlightjs';
+
+Vue.use(VueHighlightJS);
 
 Vue.config.productionTip = false;
 
