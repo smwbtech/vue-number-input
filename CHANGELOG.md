4af83138bde9dcd7864480c63b1edf8c41cd26d3
smwbtech <smwb.tech@gmail.com>
2020-02-22 13:49:36 +0300

    Fixed: decrease bundle size
    * Updated: dev deps
    * Fixed: bundle size is decreased

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.7] - 2020-02-22
+
+### Fixed
+
+-   Decrease bundle size
+
 ## [1.0.6] - 2019-08-29
 
 ### Fixed
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+const presets = [
+	[
+		'@vue/babel-preset-app',
+		{
+			useBuiltIns: process.env.NODE_ENV === 'production' ? false : 'usage'
+		}
+	]
+];
+
+if (process.env.NODE_ENV === 'production')
+	presets.unshift([
+		'minify',
+		{
+			builtIns: false
+		}
+	]);
+
 module.exports = {
-  presets: ["@vue/app"]
+	presets,
+	plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime']
 };
